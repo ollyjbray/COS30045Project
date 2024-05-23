e838e91d986f5ce24d409ed19de3a87098454418
Oliver Bray <52276965+ollyjbray@users.noreply.github.com>
2024-05-23 11:37:25 +1000

    Add files via upload

--- Dataset3_HealthExpenditure/script.js
+++ Dataset3_HealthExpenditure/script.js
@@ -1,6 +1,6 @@
 function init() {
     // set up the SVG canvas dimensions
-    const margin = { top: 50, right: 100, bottom: 100, left: 150 },
+    var margin = { top: 50, right: 100, bottom: 100, left: 150 },
         w = 1000 - margin.left - margin.right,
         h = 800 - margin.top - margin.bottom; // increased height for larger boxes
 
@@ -10,7 +10,7 @@ function init() {
             d.Value = +d.Value;
         });
 
-        const svg = d3.select("#chart")
+        var svg = d3.select("#chart")
             .append("svg")
             .attr("width", w + margin.left + margin.right + 100)
             .attr("height", h + margin.top + margin.bottom)
@@ -18,18 +18,49 @@ function init() {
             .attr("transform", `translate(${margin.left},${margin.top})`);
 
         // create the scales
-        const xScale = d3.scaleBand()
+        var xScale = d3.scaleBand()
             .range([0, w])
             .domain(data.map(d => d.Year))
             .padding(0.1); // reduced padding for larger boxes
 
-        const yScale = d3.scaleBand()
+        var yScale = d3.scaleBand()
             .range([h, 0])
             .domain(data.map(d => d.Country))
             .padding(0.1); // reduced padding for larger boxes
 
-        const colorScale = d3.scaleSequential(d3.interpolateYlGnBu)
+        var colorScale = d3.scaleSequential(d3.interpolateYlGnBu)
             .domain([0, d3.max(data, d => d.Value)]);
+
+        var Tooltip = d3.select("#chart")
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("position", "absolute")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "2px")
+            .style("border-radius", "5px")
+            .style("padding", "5px");
+
+        // Three functions that change the tooltip when user hover / move / leave a cell
+        var mouseover = function(event, d) {
+            Tooltip.style("opacity", 1);
+            d3.select(this)
+                .style("stroke", "black")
+                .style("opacity", 1);
+        };
+        var mousemove = function(event, d) {
+            Tooltip
+                .html("Country: " + d.Country + "<br>Year: " + d.Year + "<br>Value: " + d.Value.toFixed(2) + "%")
+                .style("left", (event.pageX + 10) + "px") // 10px offset to the right of the cursor
+                .style("top", (event.pageY - 20) + "px"); // 20px offset above the cursor
+        };
+        var mouseleave = function(event, d) {
+            Tooltip.style("opacity", 0);
+            d3.select(this)
+                .style("stroke", "none")
+                .style("opacity", 0.8);
+        };
 
         // append the rectangles for the heatmap
         svg.selectAll()
@@ -40,7 +71,10 @@ function init() {
             .attr("y", d => yScale(d.Country))
             .attr("width", xScale.bandwidth())
             .attr("height", yScale.bandwidth())
-            .style("fill", d => colorScale(d.Value));
+            .style("fill", d => colorScale(d.Value))
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("mouseleave", mouseleave);
 
         // add the x-axis
         svg.append("g")
@@ -59,32 +93,18 @@ function init() {
             .style("font-family", "Oswald, sans-serif")
             .style("font-size", "14px");
 
-        // add labels to the heatmap cells
-        // svg.selectAll(".text")
-        //     .data(data, d => d.Country + ':' + d.Year)
-        //     .enter()
-        //     .append("text")
-        //     .attr("x", d => xScale(d.Year) + xScale.bandwidth() / 2)
-        //     .attr("y", d => yScale(d.Country) + yScale.bandwidth() / 2)
-        //     .attr("dy", ".35em")
-        //     .attr("text-anchor", "middle")
-        //     .text(d => d.Value.toFixed(2))
-        //     .style("fill", "black")
-        //     .style("font-size", "12px")
-        //     .style("font-family", "Oswald, sans-serif")
-
         // add color key/legend
-        const legendWidth = 20;
-        const legendHeight = h;
-        const legend = svg.append("g")
+        var legendWidth = 20;
+        var legendHeight = h;
+        var legend = svg.append("g")
             .attr("class", "legend")
             .attr("transform", `translate(${w + 40}, 0)`);
 
-        const legendScale = d3.scaleLinear()
+        var legendScale = d3.scaleLinear()
             .range([legendHeight, 0])
             .domain([0, d3.max(data, d => d.Value)]);
 
-        const numBoxes = 100; // number of color boxes in the legend
+        var numBoxes = 100; // number of color boxes in the legend
         legend.selectAll("rect")
             .data(d3.range(numBoxes), d => d)
             .enter()
@@ -95,9 +115,9 @@ function init() {
             .attr("fill", d => colorScale(legendScale.invert(d * (legendHeight / numBoxes))));
 
         // add legend axis
-        const legendAxis = d3.axisRight(legendScale)
+        var legendAxis = d3.axisRight(legendScale)
             .ticks(6)
-            .tickFormat(d => d + "%")
+            .tickFormat(d => d + "%");
 
         legend.append("g")
             .attr("transform", `translate(${legendWidth}, 0)`)
